d81d1c2f1e0f86b1cee7da94b605b3f24a4aab67
BraiderZ <luisjbc2112@gmail.com>
2023-11-21 16:14:51 -0600

    Se elimina el import inecesario

--- opencv_processor.py
+++ opencv_processor.py
@@ -1,7 +1,6 @@
 import cv2
-from image_processor import ImageProcessor
 
-class OpenCVImageProcessor(ImageProcessor):
+class OpenCVImageProcessor():
     def mostrar_imagen(self, image_path):
         image = cv2.imread(image_path)
         cv2.imshow('Imagen', image)
