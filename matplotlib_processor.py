60ae56de5cf54c123a165518bf9f1563c52112a4
BraiderZ <luisjbc2112@gmail.com>
2023-11-21 15:37:49 -0600

    Se arreglo el script para que solo utilice una blibioteca

--- matplotlib_processor.py
+++ matplotlib_processor.py
@@ -1,16 +1,11 @@
 import matplotlib.pyplot as plt
-import cv2
+from matplotlib.image import imread
 from image_processor import ImageProcessor
 
 class MatplotlibImageProcessor(ImageProcessor):
     def mostrar_imagen(self, image_path):
-        image = cv2.imread(image_path)
-        plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+        image = imread(image_path)
+        plt.imshow(image)
         plt.axis('off')
         plt.show()
 
-    def procesar(self, image_path):
-
-        # Agregar logica de la funcion procesar()
-        
-        pass
