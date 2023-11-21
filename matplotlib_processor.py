576769c1e3969481027f9d729beab2386e68c29c
BraiderZ <luisjbc2112@gmail.com>
2023-11-21 16:12:27 -0600

    Se arreglo el script eliminando el import que no proporciona ayuda

--- matplotlib_processor.py
+++ matplotlib_processor.py
@@ -1,8 +1,7 @@
 import matplotlib.pyplot as plt
 from matplotlib.image import imread
-from image_processor import ImageProcessor
 
-class MatplotlibImageProcessor(ImageProcessor):
+class MatplotlibImageProcessor():
     def mostrar_imagen(self, image_path):
         image = imread(image_path)
         plt.imshow(image)
