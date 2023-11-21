59c69f706ba8b0f274b3cc34c64bbe2e8718b5ee
BraiderZ <luisjbc2112@gmail.com>
2023-11-21 16:25:42 -0600

    El main se ha modificado para que posea la forma del diagrama UML y que tenga un dise~no modular.

--- main.py
+++ main.py
@@ -1,29 +1,42 @@
 import argparse
-from opencv_processor import OpenCVImageProcessor
-from matplotlib_processor import MatplotlibImageProcessor
 
-def parse_args():
-    parser = argparse.ArgumentParser(description="Procesamiento de Imágenes")
-    parser.add_argument("--biblioteca", choices=["Matplotlib", "OpenCV"], required=True, help="Especifica la biblioteca de procesamiento de imágenes a utilizar.")
-    parser.add_argument("--imagen", required=True, help="Ruta de la imagen a procesar.")
-    return parser.parse_args()
+class ArgumentParser:
+    def __init__(self):
+        self.parser = argparse.ArgumentParser(description="Procesamiento de Imagenes")
+        self._configurar_argumentos()
+
+    def _configurar_argumentos(self):
+        self.parser.add_argument("--biblioteca", choices=["Matplotlib", "OpenCV"], required=True, help="Biblioteca para procesamiento de imagenes")
+        self.parser.add_argument("--imagen", required=True, help="Ruta de la imagen a procesar")
+
+    def parse_args(self):
+        return self.parser.parse_args()
+
+class ProcesamientoImagenes:
+    def __init__(self, args):
+        self.args = args
+
+    def procesar(self):
+        if self.args.biblioteca == 'OpenCV':
+            from opencv_processor import mostrar_imagen
+        elif self.args.biblioteca == 'Matplotlib':
+            from matplotlib_processor import mostrar_imagen
+        else:
+            print("Biblioteca no valida. Selecciona PIL, Matplotlib o OpenCV.")
+            return
+
+        try:
+            mostrar_imagen(self.args.imagen)
+        except Exception as e:
+            print(f"Error al procesar la imagen: {e}")
 
 def main():
-    args = parse_args()
+    argument_parser = ArgumentParser()
+    args = argument_parser.parse_args()
 
-    try:
-        if args.biblioteca == 'Matplotlib':
-            image_processor = MatplotlibImageProcessor()
-        elif args.biblioteca == 'OpenCV':
-            image_processor = OpenCVImageProcessor()
-        else:
-            raise ValueError("Biblioteca no válida. Selecciona Matplotlib u OpenCV.")
+    procesamiento = ProcesamientoImagenes(args)
+    procesamiento.procesar()
 
-        image_processor.mostrar_imagen(args.imagen)
-        image_processor.procesar(args.imagen)
-
-    except Exception as e:
-        print(f"Error al procesar la imagen: {e}")
-
-main()
+if __name__ == "__main__":
+    main()
 
