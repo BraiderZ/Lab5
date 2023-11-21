cc769645edd4b74d8b44388c97a242785346bf41
Emmanuel <emmanuel_aviles@Ubuntu.myguest.virtualbox.org>
2023-11-21 14:26:04 -0600

    corregir clase de opencv

---
+++ opencv_processor.py
@@ -0,0 +1,17 @@
+import cv2
+from image_processor import ImageProcessor
+
+class OpenCVImageProcessor(ImageProcessor):
+    def mostrar_imagen(self, image_path):
+        image = cv2.imread(image_path)
+        cv2.imshow('Imagen', image)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+    def procesar(self, image_path):
+
+        # Agregar logica de funcion procesar()
+
+
+        pass
+
